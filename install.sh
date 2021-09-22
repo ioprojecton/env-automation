a44de3f1d1792455f5c979bb283e189a276deb84
Nick Lediet <nlediet@northern.co>
2021-09-22 15:43:39 -0400

    Add installation script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+if [ "$(whoami)" != "root" ]; then
+    echo "Must run as root"
+
+    exit 1
+fi
+
+OS=$(cat /etc/os-release | grep ID_LIKE | cut -d '=' -f 2)
+
+case "$OS" in
+    arch)
+        yes | pacman -S ansible
+        ;;
+    debian)
+        yes | apt install ansible
+        ;;
+    *)
+        echo "We only support debian and arch based distribution"
+        exit 1
+esac
+
+ansible-playbook ansible/playbook.yml
